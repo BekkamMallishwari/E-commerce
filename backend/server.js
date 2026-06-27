ed15b8bda99c2d2ffc28a6f11a26314be99f4a19
Dev Prashant <dev.prashant1901@gmail.com>
2026-06-27 23:20:29 +0530

    Fix WebSocket CORS issue

--- backend/server.js
+++ backend/server.js
@@ -43,8 +43,6 @@ const app = express();
 const http = require("http");
 const server = http.createServer(app);
 const { initSocket } = require("./utils/socketManager");
-initSocket(server);
-
 // constants
 const PORT = Number(process.env.PORT) || 5000;
 
@@ -125,6 +123,9 @@ const allowedOrigins = [
 
     "https://www.bhuvansh.xyz"
 ];
+
+// initialize websocket server with CORS
+initSocket(server, allowedOrigins);
 
 // cors
 app.use(
